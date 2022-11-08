c9fd626954eea3020f151afa06436eb9acff0ef8
unknown <carlo.costa1@hotmail.com>
2022-11-08 11:07:21 -0500

    bug fixes

---
+++ calibration.py
@@ -0,0 +1,23 @@
+import pyautogui as autogui
+import tkinter as tk
+from tkinter import ttk
+
+
+print(autogui.position().x)
+number_of_animals = (0,0)
+dog_position = (0,0)
+sexe_male_position = (0,0)
+sexe_female_position = (0,0)
+castrer_position = (0,0)
+position_suivant_race = (0,0)
+position_suivant_age = (0,0)
+position_peu_actif = (0,0)
+position_moyen_actif = (0,0)
+position_tres_actif = (0,0)
+position_peu_maigre = (0,0)
+position_ideal_corps = (0,0)
+position_gros_coprs = (0,0)
+position_suivant_poids = (0,0)
+position_type_de_croquette = (0,0)
+position_finaliser = (0,0)
+position_vrai_prix = (0,0)
